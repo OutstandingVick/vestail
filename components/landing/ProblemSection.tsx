beb526c066daf8e7db5ae065b6785ecdbb84c12e
OutstandingVick <scottdodo2015@outlook.com>
2026-09-23 11:26:09 +0100

    Add the problem section component

---
+++ components/landing/ProblemSection.tsx
@@ -0,0 +1,6 @@
+import styles from "./ProblemSection.module.css";
+
+/** The three different claims a single ticker can represent. */
+export function ProblemSection() {
+  return <section className={styles.section} />;
+}
